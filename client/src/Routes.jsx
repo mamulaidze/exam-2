1fa0e27dcd894a84b289b5ab76f125b359bdb5e4
mamulaidze <mamulaidzeg186@gmail.com>
2025-10-31 13:42:54 +0400

    fixed

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 import Header from "./components/Header";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 
